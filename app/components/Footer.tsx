9334725140c7abf3af7eff804aa870b1ab16a03e
devonsydney <devon.boultonmills@gmail.com>
2024-08-19 11:18:39 +0100

    modifying footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full py-4 bg-gray-800 text-white mt-8">
+    <footer className="w-full py-4 bg-gray-800 text-white">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Jennifer Hazard. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Jennifer Hazard. <a href="mailto:info@jenniferhazard.com" className="text-gray-300 hover:text-white transition duration-300">info@jenniferhazard.com</a>.</p>
       </div>
     </footer>
   );
