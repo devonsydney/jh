302f92693de53a63c4b76c4a0215720f6abc8955
devonsydney <devon.boultonmills@gmail.com>
2024-12-07 17:33:52 +0000

    re-arranging footer text

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 md:bg-gray-800 text-gray-400 py-4 text-center mt-[-2px]">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Jennifer Hazard. <a href="mailto:info@jenniferhazard.com" className="text-gray-300 hover:text-white transition duration-300">info@jenniferhazard.com</a>.</p>
+        <p><a href="mailto:info@jenniferhazard.com" className="text-gray-300 hover:text-white transition duration-300">info@jenniferhazard.com</a></p>
+        <p>&copy;{new Date().getFullYear()} Jennifer Hazard</p>
       </div>
     </footer>
   );
